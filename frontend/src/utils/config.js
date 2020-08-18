1473038009d7839fab038213613e7d34a44b3443
Loek Zweers <loek@zweersdesign.com>
2020-08-18 20:19:30 +0200

    :bike: stupid typo

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-  DEV_URL: 'https://rondemaestro-test.heroku.com/api/v1/',
+  DEV_URL: 'https://rondemaestro-test.herokuapp.com/api/v1/',
   // DEV_URL: 'http://localhost:1992/api/v1/',
   PROD_URL: 'https://rondemaestro-test.heroku.com/api/v1/',
 };
