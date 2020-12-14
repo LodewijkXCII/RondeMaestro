0ad4c56389a5e40a9c044013d10c21e46a5ab150
LodewijkXCII <loek@zweersdesign.com>
2020-12-14 17:53:38 +0100

    :bike: update

--- backend/src/api/teams/teams.queries.js
+++ backend/src/api/teams/teams.queries.js
@@ -2,25 +2,32 @@ const db = require('../../db');
 const tableNames = require('../../constants/tableNames');
 
 const fields = [
-  'id',
-  'name',
-  'abbreviation',
-  'country_id',
-  'level',
-  'year',
-  'image_url',
+  'team.id',
+  'team.name',
+  'team.abbreviation',
+  'country.abbreviation as country',
+  'team.level',
+  'team.year',
+  'team.image_url',
 ];
 
 module.exports = {
   find() {
-    return db(tableNames.team).select(fields).where('year', '2020');
+    return db(tableNames.team)
+      .select(fields)
+      .join('country', 'team.country_id', 'country.id')
+      .where('year', '2020')
+      .orderBy('id', 'asc');
   },
   async get(id) {
     return db(tableNames.team)
       .select(fields)
+      .join('country', 'team.country_id', 'country.id')
       .where({
-        id,
+        'team.id': id,
       })
       .first();
   },
+
+  //TODO Make put query
 };
